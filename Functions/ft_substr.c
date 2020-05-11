c837ec5850819ea38c011632d493b3076eac8ed9
Diana Salamanca <dsalaman@Dianas-MacBook-Pro.local>
2020-05-11 16:19:10 +0200

    Update substr, protect malloc & diff between printf is const char

--- Functions/ft_substr.c
+++ Functions/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   ft_substr.c                                        :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: dsalaman <marvin@codam.nl>                   +#+                     */
+/*   By: dsalaman <dsalaman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/20 11:19:03 by dsalaman      #+#    #+#                 */
-/*   Updated: 2019/11/20 11:19:05 by dsalaman      ########   odam.nl         */
+/*   Updated: 2020/05/11 15:50:52 by dsalaman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@ char				*ft_substr(char const *s, unsigned int start, size_t len)
 	if (length - start < len)
 		len = length - start;
 	string = (char *)malloc(sizeof(char) * (len + 1));
+	if (!string)
+		return (NULL);
 	i = 0;
-	while (s[start] != '\0' && i < len)
+	while (s[start] != '\0' && i < len && length > start)
 	{
 		string[i] = s[start];
 		start++;
